883232dd8ed72bd76622e703f1f3eed5e15c1bb0
mktange <mkt@delegate.dk>
2016-04-15 15:46:40 +0200

    Updated RELEASE_NOTES.md and version

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,5 +1,8 @@
 # Release Notes
 
+### 1.2.2 - April 15 2016
+* Added support for entities and global option sets sharing the same name
+
 ### 1.2.1 - April 7 2016
 * Fixed PerformAsBulk sending too many requests
 * Ensures distinct entity logical names
--- src/XrmContext/AssemblyInfo.fs
+++ src/XrmContext/AssemblyInfo.fs
@@ -6,9 +6,9 @@ open System.Reflection
 [<assembly: AssemblyDescriptionAttribute("Tool to generate early-bound .NET framework classes and enumerations for MS CRM Dynamics server-side coding.")>]
 [<assembly: AssemblyCompanyAttribute("Delegate A/S")>]
 [<assembly: AssemblyCopyrightAttribute("Copyright (c) Delegate A/S 2016")>]
-[<assembly: AssemblyVersionAttribute("1.2.1")>]
-[<assembly: AssemblyFileVersionAttribute("1.2.1")>]
+[<assembly: AssemblyVersionAttribute("1.2.2")>]
+[<assembly: AssemblyFileVersionAttribute("1.2.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.2.1"
+    let [<Literal>] Version = "1.2.2"
