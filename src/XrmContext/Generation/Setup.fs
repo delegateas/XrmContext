3b6b2af0c3dbdb40f274203ab9eb047179a41774
Magnus Gether Sørensen <magesoe@gmail.com>
2020-03-13 11:08:23 +0100

    Added missing parameter 'labelMapping'

--- src/XrmContext/Generation/Setup.fs
+++ src/XrmContext/Generation/Setup.fs
@@ -78,7 +78,7 @@ let interpretCrmData (gSettings: XcGenerationSettings) out sdkVersion (rawState:
 
   let entityMetadata =
     publicEntities
-    |> Array.Parallel.map (interpretEntity entityNames entityMap entityToIntersects gSettings.deprecatedPrefix sdkVersion)
+    |> Array.Parallel.map (interpretEntity entityNames entityMap entityToIntersects gSettings.deprecatedPrefix gSettings.labelMapping sdkVersion)
     |> Array.sortBy (fun x -> x.schemaName)
 
 
